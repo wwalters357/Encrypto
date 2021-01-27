d2ded6639e406bc5fac5a1ae2cad1846d84b7b27
William Walters <wwalters357@gmail.com>
2021-01-26 23:07:36 -0500

    Added Cipher classes with some shell methods for Caesar ciphers.

---
+++ Encrypto/Encrypto/Cipher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encrypto
+{
+    class Cipher
+    {
+        public Cipher(string plainText, bool encrypt)
+        {
+            PlainText = plainText;
+            Encrypt = encrypt;
+        }
+
+        public string Caesar_Cipher(int key)
+        {
+            return null;
+        }
+
+        public string Double_Caesar_Cipher(string key)
+        {
+            return null;
+        }
+
+        // Calculate modulus division
+        private int Mod(int x, int b)
+        {
+            return (x < 0) ? (x + b) % b : x % b;
+        }
+
+        // --------------------------------------------------------------------
+        // ------------------- Accessor Methods -------------------------------
+        // --------------------------------------------------------------------
+        public string PlainText { get; }
+        public bool Encrypt { get; }
+    }
+}
