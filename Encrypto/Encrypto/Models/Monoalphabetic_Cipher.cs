eb5ea4f5fcfeefd367855e71a582b94366069020
RonFMitchell <ron.mitchell810@gmail.com>
2021-02-01 19:45:51 -0500

    Created Is_Key_Valid function for monoalphabetic cipher and corresponding testing

--- Encrypto/Encrypto/Models/Monoalphabetic_Cipher.cs
+++ Encrypto/Encrypto/Models/Monoalphabetic_Cipher.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Encrypto
 {
-    class Monoalphabetic_Cipher : Cipher
+    public class Monoalphabetic_Cipher : Cipher
     {
-        public Monoalphabetic_Cipher(string message, string key) : base(message, key)
+		public Monoalphabetic_Cipher(string message, string key) : base(message, key)
         {
 
         }
-        public override string Decrypt()
+        
+		public override string Decrypt()
         {
             // return Substitution(Key, false);
             throw new NotImplementedException();
@@ -18,17 +20,78 @@ namespace Encrypto
 
         public override string Encrypt()
         {
-            return Substitution(Key, true);
+            return Monoalphabetic_Substitution(Message, Key, true);
         }
 
-        public override bool Is_Key_Valid()
+		// Checks the key to make sure it is 26 characters, all letters and has no duplicates
+		public override bool Is_Key_Valid()
         {
-            throw new NotImplementedException();
-        }
+			if (Key.Length != 26)
+			{
+				return false;
+			}
+			for (int i = 0; i < Key.Length; i++)
+			{
+				if (!Char.IsLetter(Key[i]))
+				{
+					return false;
+				}
+				for (int j = i + 1; j < Key.Length; j++)
+				{
+					if (Key[i] == Key[j])
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
 
-        private string Substitution(string key, bool encryptMesage)
+		// Store each element of key into a dictionary for fast access
+		// Key must be length 26 to have a substitution for each letter
+		// Must check for valid key
+		private string Monoalphabetic_Substitution(string input, string key, bool encryptMessage)
         {
-            throw new NotImplementedException();
-        }
+			string output = "";
+			// Create key map
+			var keyMap = new Dictionary<int, char>();
+			for (int i = 0; i < key.Length; i++)
+			{
+				keyMap.Add(i, key[i]);
+			}
+
+			if (encryptMessage)
+			{
+				for (int i = 0; i < input.Length; i++)
+				{
+					if (Char.IsLetter(input[i]))
+					{
+						char c = keyMap[Get_Alphabetic_Value(input[i])];
+						output += (char)((Char.IsUpper(input[i])) ? c : c + 'a' - 'A');
+					}
+					else
+					{
+						output += input[i];
+					}
+				}
+			}
+			else
+			{
+				for (int i = 0; i < input.Length; i++)
+				{
+					if (Char.IsLetter(input[i]))
+					{
+						// Find current char in map
+						int x = keyMap.FirstOrDefault(y => y.Value == Char.ToUpper(input[i])).Key;
+						output += (char)((Char.IsUpper(input[i])) ? x + (int)'A' : x + (int)'a');
+					}
+					else
+					{
+						output += input[i];
+					}
+				}
+			}
+			return output;
+		}
     }
 }
---
+++ Encrypto/EncryptoTests/Models/Monoalphabetic_CipherTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Encrypto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encrypto.Tests
+{
+    [TestClass()]
+    public class Monoalphabetic_CipherTests
+    {
+        [TestMethod()]
+        public void Monoalphabetic_CipherTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DecryptTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EncryptTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Is_Key_ValidTest()
+        {
+            Cipher cipher = new Monoalphabetic_Cipher("message", "QWERTYUIOPLKJHGFDSAZXCVBNM");
+            Assert.IsTrue(cipher.Is_Key_Valid());
+            cipher = new Monoalphabetic_Cipher("message", "QWERTYUQOPLKJHGFDSAZQCVBNM");
+            Assert.IsFalse(cipher.Is_Key_Valid());
+            cipher = new Monoalphabetic_Cipher("message", "QWERTYU");
+            Assert.IsFalse(cipher.Is_Key_Valid());
+            cipher = new Monoalphabetic_Cipher("message", "QWERTYUIOP2KJHGFDSAZXCVBNM");
+            Assert.IsFalse(cipher.Is_Key_Valid());
+        }
+    }
+}
